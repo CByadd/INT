46f546b44ff18c3c4f1df6334af4f063b71bbe59
Srinivash Dev <82706452+CByadd@users.noreply.github.com>
2023-10-28 17:38:06 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Sidebar() {
   return (
     <div className='h-11 w-11 SIDE'><div></div>
-        <button className='H'><img src='/Home.png'/></button>
-        <button className='S'><img src='/Search.png'/></button>
+        <button className='H'><img src='Home.png'/></button>
+        <button className='S'><img src='Search.png'/></button>
         <button className='hidden md:block'><img src='/Explore.png'/></button>
         <button className='R'><img src='/Reels.png'/></button>
         <button className='ml-1 C'><img src='/Chat.png'/></button>
