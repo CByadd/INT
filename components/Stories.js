1477e257230fa417cef3eaf83f9cbf5362aa6bb7
Srinivash Dev <82706452+CByadd@users.noreply.github.com>
2023-10-28 17:51:18 +0530

    Update Stories.js

--- components/Stories.js
+++ components/Stories.js
@@ -7,27 +7,27 @@ function Stories() {
     <div className='mx-auto flex justify-center'>
  <div className='relative   h-24 w-24  mt-2'>
  <Image
-        src={'/1.png'} layout='fill' objectFit='contain'/>
+        src={'1.png'} layout='fill' objectFit='contain'/>
  </div>
  <div className='relative  h-24 w-24 m-0.5 mt-2'>
  <Image
-        src={'/2.png'} layout='fill' objectFit='contain'/>
+        src={'2.png'} layout='fill' objectFit='contain'/>
  </div>
  <div className='relative hidden md:block h-24 w-24 m-0.5 mt-2'>
  <Image
-        src={'/3.png'} layout='fill' objectFit='contain'/>
+        src={'3.png'} layout='fill' objectFit='contain'/>
  </div>
  <div className='relative  h-24 w-24 m-1 mt-2'>
  <Image
-        src={'/4.png'} layout='fill' objectFit='contain'/>
+        src={'4.png'} layout='fill' objectFit='contain'/>
  </div>
  <div className='relative  h-24 w-24 m-1 mt-2'>
  <Image
-        src={'/5.png'} layout='fill' objectFit='contain'/>
+        src={'5.png'} layout='fill' objectFit='contain'/>
  </div>
  <div className='relative  h-24 w-24 m-1 mt-2'>
  <Image
-        src={'/6.png'} layout='fill' objectFit='contain'/>
+        src={'6.png'} layout='fill' objectFit='contain'/>
  </div>
  
  </div>
